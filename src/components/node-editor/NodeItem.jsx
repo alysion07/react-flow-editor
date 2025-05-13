840f0a98a6c9f77e8deb03defa84366f1ad1fb83
한철민 <alysion@e8ight.co.kr>
2025-05-13 18:06:52 +0900

    init commit

--- src/components/node-editor/NodeItem.jsx
+++ src/components/node-editor/NodeItem.jsx
@@ -7,22 +7,24 @@ import ICO from '../../../icon/keyboard_command_key_24dp_E3E3E3_FILL0_wght400_GR
 const NodeItem = ({ data, xPos, yPos, type, onDelete }) => {
     return (
         <div className="node">
-            <div className="node-header">
-                {/*<div className="node-icon">{componentDef.icon}</div>*/}
-                <div className="position-node-label">{data.label}</div>
-                <div className="node-title">
-                    {/*{node.name || componentDef.label}*/}
+                <div className={`node-header-${data.label}`}>
+             <div className={'node-header'}>
+                    {/*<div className="node-icon">{componentDef.icon}</div>*/}
+                    <div className="position-node-label">{data.label}</div>
+                    <div className="node-title">
+                        {/*{node.name || componentDef.label}*/}
+                    </div>
+                    <button
+                        className="node-delete-btn"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            if (onDelete) onDelete();
+                            // TODO : 삭제기능 구현 필요
+                        }}
+                    >
+                        ×
+                    </button>
                 </div>
-                <button
-                    className="node-delete-btn"
-                    onClick={(e) => {
-                        e.stopPropagation();
-                         if (onDelete) onDelete();
-                        // TODO : 삭제기능 구현 필요
-                    }}
-                >
-                    ×
-                </button>
             </div>
             {/* node body */ }
             <div className="node-body">
